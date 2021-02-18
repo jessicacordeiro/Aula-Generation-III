06fcb20d8628b3ae010dfb3f16f2c7c689122b24
Silvio <jessica.cordeiro121@gmail.com>
2021-02-18 02:52:54 -0300

    Atividade em CRUD Back-end

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,64 @@
+package org.generation.blogPessoal.model;
+
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import com.sun.istack.NotNull;
+
+@Entity
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 1000)
+	private String titulo;
+	
+	@NotNull
+	@Size(min = 10, max = 500)
+	private String texto;
+	
+	
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getTexto() {
+		return texto;
+	}
+	
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
